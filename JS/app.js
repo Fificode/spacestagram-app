1936fb862068a56b50339c3f1c0bd1be06f715c4
Firdaws <lamidifirdaws@gmail.com>
2022-01-10 22:43:46 +0100

    first commit

---
+++ JS/app.js
@@ -0,0 +1,44 @@
+const apiKey = "dRubRkZc5TtGtdoG8GdGOaGDjGigGE5Pom6trrZB";
+const submitBtn = document.getElementById("search-button");
+const searchForm = document.getElementById("search-form");
+const resultContainer = document.getElementById("result-container");
+const input = document.getElementById("search");
+// api_key=${apiKey}
+
+
+const getRequest = async (e) => {
+    e.preventDefault();
+    let response = await fetch(`https://images-api.nasa.gov/search?q=${input.value}`,
+        {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+            },
+        }
+
+    );
+    if (!response.ok) {
+        throw Error("ERROR");
+    }
+    response = await response.json();
+    console.log(response);
+
+}
+
+searchForm.addEventListener("submit", getRequest
+);
+
+const renderResult = async (e) => {
+    let result = await getRequest();
+
+
+    result.photos.forEach((photo) => {
+        let pic = document.createElement("div");
+        pic.innerHTML = `<img src="${photo.collection.items[4].href}">`
+            ;
+
+    }
+    )
+    resultContainer.appendChild(pic);
+};
+renderResult();
